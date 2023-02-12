d09b91ff278c3e379a5d049e5f79e29e5f0e9ce4
Margarita <margarita.levitm@checkmarx.com>
2023-02-12 20:58:11 +0200

    Add files via upload

--- JavaClassA.java
+++ JavaClassA.java
@@ -11,15 +11,11 @@ public class JavaClassA {
     public static void main(String[] args) {
         try {
             ScriptEngineManager manager = new ScriptEngineManager();
-            ScriptEngine engine = manager.getEngineByName("JavaScript");
-  //          System.out.println(args[0]);
-            engine.eval("print('"+ args[0] + "')");
+          //  ScriptEngine engine = manager.getEngineByName("JavaScript");
+            System.out.println(args[0]);
+          //  engine.eval("print('"+ args[0] + "')");
         } catch(Exception e) {
             e.printStackTrace();
         }
     }
 }
-
-
-
-
