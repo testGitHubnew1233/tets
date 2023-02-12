878041627fca430261487cf62fff200afbf610c9
Margarita <margarita.levitm@checkmarx.com>
2023-02-12 20:12:58 +0200

    Update JavaClassA.java

--- JavaClassA.java
+++ JavaClassA.java
@@ -12,7 +12,7 @@ public class JavaClassA {
         try {
             ScriptEngineManager manager = new ScriptEngineManager();
             ScriptEngine engine = manager.getEngineByName("JavaScript");
-            System.out.println(args[0]);
+  //          System.out.println(args[0]);
             engine.eval("print('"+ args[0] + "')");
         } catch(Exception e) {
             e.printStackTrace();
